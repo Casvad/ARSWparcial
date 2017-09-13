125dfd426ac9fa5b46d5420d39c8a943d45de4d3
Héctor Cadavid <hector.cadavid@gmail.com>
2017-09-13 11:01:16 -0500

    first commit

--- src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
@@ -18,8 +18,14 @@ public class HostBlackListsValidator {
     private static final int BLACK_LIST_ALARM_COUNT=5;
     
     /**
-     * 
-     * @param ipaddress 
+     * Check the given host's IP address in all the available black lists,
+     * and report it as NOT Trustworthy when such IP was reported in at least
+     * BLACK_LIST_ALARM_COUNT lists, or as Trustworthy in any other case.
+     * The search is not exhaustive: When the number of occurrences is equal to
+     * BLACK_LIST_ALARM_COUNT, the search is finished, the host reported as
+     * NOT Trustworthy, and the list of the five blacklists returned.
+     * @param ipaddress suspicious host's IP address.
+     * @return  Blacklists numbers where the given host's IP address was found.
      */
     public List<Integer> checkHost(String ipaddress){
         
@@ -29,10 +35,11 @@ public class HostBlackListsValidator {
         
         HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
         
-        for (int i=0;i<skds.getRegisteredServersCount() && ocurrencesCount<5;i++){
+        for (int i=0;i<skds.getRegisteredServersCount() && ocurrencesCount<BLACK_LIST_ALARM_COUNT;i++){
             if (skds.isInBlackListServer(i, ipaddress)){
                 
                 blackListOcurrences.add(i);
+                
                 ocurrencesCount++;
             }
         }
